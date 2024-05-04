2c4285b75b941b68d11b9c497798c142e8c4b6a6
Godfred Nai <godfrednai@gmail.com>
2024-05-04 01:22:23 +0000

    minor changes

---
+++ app/(auth)/sign-up.jsx
@@ -0,0 +1,57 @@
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
+
+const SignUp = () => {
+  const [form, setForm] = useState({ username: "", email: "", password: "" });
+  const [isSubmitting, setisSubmitting] = useState(false);
+  const submit = () => {};
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[80vh] px-6 my-6">
+          <Image source={images.logo} resizeMode="contain" className="w-[115px] h-[35px]" />
+          <Text className="text-2xl text-white font-psemibold mt-10">Sign Up to Aora</Text>
+          <FormField
+            title="Usename"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-10"
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">Already got an account?</Text>
+            <Link href="/sign-in" className="text-lg text-secondary font-psemibold">
+              Sign in
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
---
+++ app/index.jsx
@@ -0,0 +1,44 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Redirect, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, Image } from "react-native";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image source={images.logo} resizeMode="contain" className="w-[130px] h-[84px]" />
+          <Image
+            source={images.cards}
+            resizeMode="contain"
+            className="max-w-[380px] h-[300px] w-full"
+          />
+          <View className="relative mt-5">
+            <Text className="text-4xl text-white font-bold text-center">
+              Discover Endless Posibilities with <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              resizeMode="contain"
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless exploration with
+            Aora
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+}
